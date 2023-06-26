6b28eb04c7b97d204276ffe5a53d2de3bafc4c6a
tttptg <735343084@qq.com>
2023-06-26 13:17:04 +0800

    init front project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import ChooseFloder from './pages/chooseFloder';
+import Result from './pages/result';
+
+function App() {
+  return (
+    <ChooseFloder/>
+  );
+}
+
+export default App;
---
+++ src/pages/chooseFloder/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class ChooseFloder extends Component {
+    render() {
+        return (
+            <h1>ChooseFloder</h1>
+        )
+    }
+}
