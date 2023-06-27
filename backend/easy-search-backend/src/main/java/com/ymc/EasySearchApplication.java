bf069ebdb67a595cb68cdf70c5740ba20ef445e6
tttptg <735343084@qq.com>
2023-06-27 09:51:40 +0800

    add springboot dependencies

---
+++ backend/easy-search-backend/src/main/java/com/ymc/EasySearchApplication.java
@@ -0,0 +1,11 @@
+package com.ymc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasySearchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EasySearchApplication.class,args);
+    }
+}
