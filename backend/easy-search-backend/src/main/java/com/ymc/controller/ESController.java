b6e789211239dbab3d4aada32f2d3ef24d0dd5af
tttptg <735343084@qq.com>
2023-06-30 00:05:27 +0800

    最终版本

--- backend/easy-search-backend/src/main/java/com/ymc/controller/ESController.java
+++ backend/easy-search-backend/src/main/java/com/ymc/controller/ESController.java
@@ -22,10 +22,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 @RequestMapping("/es")
