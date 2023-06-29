9c5f94e806eaa5ca0393ec5089aff586880ec52d
tttptg <735343084@qq.com>
2023-06-29 23:00:07 +0800

    整体基本完成，仅剩下下载和一些页面效果完善

--- backend/easy-search-backend/src/main/java/com/ymc/controller/ESController.java
+++ backend/easy-search-backend/src/main/java/com/ymc/controller/ESController.java
@@ -6,16 +6,26 @@ import com.ymc.util.R;
 import org.apache.http.HttpHost;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightField;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/es")
@@ -56,6 +66,73 @@ public class ESController {
             }
         }
         return R.success(null);
+    }
+
+    @PostMapping("search")
+    @CrossOrigin
+    public String search(@RequestParam("keyword") String keyword){
+        System.out.println(keyword);
+        StringBuilder result = new StringBuilder();
+        client = new RestHighLevelClient(RestClient.builder(
+                HttpHost.create("http://192.168.48.128:9200")
+        ));
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.query(QueryBuilders.matchQuery("content", keyword));
+
+        HighlightBuilder highlightBuilder = new HighlightBuilder();
+        highlightBuilder.field("content");
+        highlightBuilder.preTags("<strong>");
+        highlightBuilder.postTags("</strong>");
+        searchSourceBuilder.highlighter(highlightBuilder);
+
+        SearchRequest searchRequest = new SearchRequest("test");
+        searchRequest.source(searchSourceBuilder);
+
+
+        try {
+            SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
+            Map<String, List<String>> resultMap = new HashMap<>();
+
+            for (SearchHit hit : searchResponse.getHits().getHits()) {
+                String filePath = hit.getSourceAsMap().get("path").toString();
+                String lineContent = hit.getSourceAsMap().get("content").toString();
+                int lineNumber = Integer.parseInt(hit.getSourceAsMap().get("line").toString());
+                // 获取高亮结果
+                Map<String, HighlightField> highlightFields = hit.getHighlightFields();
+                if (!CollectionUtils.isEmpty(highlightFields)) {
+                    // 根据字段名获取高亮结果
+                    HighlightField highlightField = highlightFields.get("content");
+                    if (highlightField != null) {
+                        // 获取高亮值
+                        // 覆盖非高亮结果
+                        lineContent = highlightField.getFragments()[0].string();
+                    }
+                }
+                if (!resultMap.containsKey(filePath)) {
+                    resultMap.put(filePath, new ArrayList<>());
+                }
+
+                List<String> lines = resultMap.get(filePath);
+                lines.add("Line " + lineNumber + "\t" + lineContent);
+            }
+
+            for (Map.Entry<String, List<String>> entry : resultMap.entrySet()) {
+                String filePath = entry.getKey();
+                List<String> lines = entry.getValue();
+
+                result.append(filePath).append("<br>");
+                result.append("-----------------------------------").append("<br>");
+                for (String line : lines) {
+                    result.append(line).append("<br>");
+                }
+                result.append("<br>");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result.toString();
     }
 
     public void insert(String path,String content,int line){
--- front/src/components/search/index.js
+++ front/src/components/search/index.js
@@ -1,13 +1,34 @@
 import React, {Component} from "react";
 import './index.css';
+import axios from "axios";
 
 export default class Search extends Component {
+    state = {
+        inputValue:"",
+    }
+
+
+    handleChangeInputValue = (event)=>{
+        console.log(event.target.value);
+        this.setState({
+            inputValue:event.target.value
+        })
+    }
+
+    handleSearch = ()=>{
+        axios.post("http://localhost:8080/es/search?keyword="+this.state.inputValue).then(response =>{
+            // console.log(response.data)
+            this.props.handleSearchResult(response.data)
+        })
+    }
+
     render() {
+
         return (
             <div id="search-bar">
                 <img src="./images/easy-search-logo.png" alt="1" className="small-logo"/>
-                <input className='input-bar' type="text" placeholder="搜索框"/>
-                <button className='botton-search'>开始检索</button>
+                <input className='input-bar' type="text" placeholder="搜索框" onChange={this.handleChangeInputValue}/>
+                <button className='botton-search' onClick={this.handleSearch}>开始检索</button>
             </div>
         );
     }
